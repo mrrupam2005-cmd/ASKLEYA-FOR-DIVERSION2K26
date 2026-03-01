1b3b7d66da901d1d6d581a9d70debbf17323326a
RUPAM MANDAL <mrrupam2005@gmail.com>
2026-03-01 09:57:39 +0530

    Add files via upload

---
+++ build/chunks/[turbopack-node]_transforms_postcss_ts_9cd9e039._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/OneDrive/Documents/GitHub/askleya-2/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/b6df1_a7f6147f._.js",
+  "chunks/[root-of-the-server]__940f85d0._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/OneDrive/Documents/GitHub/askleya-2/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
